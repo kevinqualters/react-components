2ecc649ca0744d3e2be22d30a817f7b7ccdee12f
Jason Johnson <jjohnson@dataminr.com>
2015-03-13 17:03:27 -0600

    PortalMixins unit tests.

---
+++ src/js/mixins/tests/PortalMixins.test.js
@@ -0,0 +1,28 @@
+define(function(require) {
+    'use strict';
+
+    var PortalMixins = require('drc/mixins/PortalMixins');
+    var React = require('react');
+    var TestUtils = React.addons.TestUtils;
+
+    describe('PortalMixins', function() {
+        describe('openPortal function', function() {
+            it('should open a portal and set the portalNode', function() {
+                PortalMixins.openPortal(<div id="portal"></div>);
+
+                expect(PortalMixins.portalNode.childNodes[0].id).toEqual('portal');
+            });
+        });
+
+        describe('closePortal function', function() {
+            it('should close the portal if the portal is open and not throw an error if the portal was already closed', function() {
+                PortalMixins.closePortal();
+
+                expect(this.portalNode).toBeUndefined();
+                delete(this.portalNode);
+
+                expect(function() {PortalMixins.closePortal();}).not.toThrow();
+            });
+        });
+    });
+});
