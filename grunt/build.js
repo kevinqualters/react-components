edf2dd0aa932f60cf7c386813f955cde12a13015
Jason Johnson <jjohnson@dataminr.com>
2015-04-16 17:01:16 -0600

    Added bower ignore and stopped mangling of minified files.

--- grunt/build.js
+++ grunt/build.js
@@ -4,6 +4,9 @@ module.exports = function(grunt, options) {
     return {
         tasks: {
             uglify: {
+                options: {
+                    mangle: false
+                },
                 min: {
                     files: grunt.file.expandMapping(['src/compiled/**/*.js',
                                                      '!src/compiled/**/*.test.js',
